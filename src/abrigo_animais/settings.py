1887204888f24e2bfadcd1b347c83b90c9403d21
Camila Lima <camilam.oliveiralima@gmail.com>
2024-05-26 18:50:14 -0300

    update settings

--- src/abrigo_animais/settings.py
+++ src/abrigo_animais/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,7 +144,6 @@ LOCALE_PATHS = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (Images, Videos, etc)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#media-root
